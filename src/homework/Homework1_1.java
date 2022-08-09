858a67522645c8b727e775577cb47077ad331861
19_Dennis <dennis905078@gmail.com>
2022-08-09 10:59:04 +0800

    feat: Homework1_1.java

---
+++ src/homework/Homework1_1.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class Homework1_1 {
+
+//請設計一隻Java程式,計算12,6這兩個數值的和與積
+	public static void main(String[] args) {
+		
+		int numA = 12;
+		int numB = 6;
+		int sum =  numA + numB;
+		int product = numA * numB;
+		
+		System.out.println("sum="+ sum);
+		System.out.println("product=" + product);
+	}
+}
